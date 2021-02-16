c94e450d945cd0a8e6091d63af1455b7cb41ae45
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2021-02-16 14:21:51 -0300

    ajout des produits depuis l'API commercejs

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {Grid} from '@material-ui/core'
 import  Product  from './Product/Product'
+import useStyles from './styles'
 
 
 
-const products = [
-    {id:1, name: 'Macbook', description: 'Apple Macbook Pro',  price: 23, image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-silver-select-201810?wid=892&hei=820&&qlt=80&.v=1603332212000'},
-    {id:2, name: 'Macbook', description: 'Apple Macbook Pro',  price: 23, image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-silver-select-201810?wid=892&hei=820&&qlt=80&.v=1603332212000'},
-]
 
-const Products = () => {
+const Products = ({ products}) => {
 
+    const classes = useStyles()
 
     return(
-        <main>
+        <main className={ classes.content}>
+        <div className={classes.toolbar}></div>
             <Grid container justify='center' spacing={4}>
                 {products.map((product) =>(
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
